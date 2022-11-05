650106d354fd9b3e2f3a0c46762aa36410e1adaa
Aleksey Lisun <insuline.ru@gmail.com>
2022-11-06 00:48:37 +0300

    ✨ add metadata handler

--- constants/index.js
+++ constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x4B8a729efE0cBC8da76d9841E853E3bd38724795";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://whitelist-frontend-insulineru.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
